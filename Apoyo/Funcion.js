54dc5668e4f0bf9d2711acb60545988567dce429
felixgims <felixgims@outlook.com>
2021-02-22 23:18:30 +0100

    ok

--- Apoyo/Funcion.js
+++ Apoyo/Funcion.js
@@ -1,21 +1,23 @@
 function leerpdf (referencia,ruta){
-    fetch(ruta,{mode: 'cors'})
-    .then(response => response.text())
-    .then(pagina => {
-        if(document.getElementById(referencia).innerHTML == "Abrir Guia Docente"){
-            document.getElementById(referencia).innerHTML =  "Cerrar Guia Docente";
-            var pdf = document.createElement("embed");
-            pdf.setAttribute("src", ruta);
-            pdf.setAttribute("type","application/pdf");
-            pdf.setAttribute("width","90%");
-            pdf.setAttribute("height","550rem");
-            var id = referencia + "pdf";
-            pdf.setAttribute("id", id);
-            document.getElementById(referencia).insertAdjacentElement("beforebegin",pdf);
-        }else{
-            var id = referencia + "pdf";
-            document.getElementById(referencia).innerHTML = "Abrir Guia Docente";
-            document.getElementById(id).remove();
+    fetch(ruta)
+    .then(response => {
+        if (response.ok){
+            if(document.getElementById(referencia).innerHTML == "Abrir Guia Docente"){
+                document.getElementById(referencia).innerHTML =  "Cerrar Guia Docente";
+                var pdf = document.createElement("embed");
+                pdf.setAttribute("src", ruta);
+                console.log(ruta);
+                pdf.setAttribute("type","application/pdf");
+                pdf.setAttribute("width","90%");
+                pdf.setAttribute("height","550rem");
+                var id = referencia + "pdf";
+                pdf.setAttribute("id", id);
+                document.getElementById(referencia).insertAdjacentElement("beforebegin",pdf);
+            }else{
+                var id = referencia + "pdf";
+                document.getElementById(referencia).innerHTML = "Abrir Guia Docente";
+                document.getElementById(id).remove();
+            }
         }
     })
         
